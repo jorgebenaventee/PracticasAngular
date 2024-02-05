d8727b581973108fb94d3389e23e57954ed687b3
Jorge Benavente <jorge.benavente@alumno.iesluisvives.org>
2024-02-05 19:14:52 +0100

    Changes

--- TalleresVives/src/app/app.component.ts
+++ TalleresVives/src/app/app.component.ts
@@ -11,6 +11,9 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  log(event: MouseEvent) {
+    console.log(event)
+  }
   title = 'TalleresVives';
   form = this.formBuilder.group({
     numeroFactura: ['', [Validators.required, Validators.minLength(5)]],
